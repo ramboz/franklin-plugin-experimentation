07d8b2692327ef974ff795b6250ee1af7798ac15
Julien Ramboz <ramboz@adobe.com>
2022-12-23 12:18:05 +0100

    fix: preview overlay

--- preview.js
+++ preview.js
@@ -89,7 +89,7 @@ async function createExperiment() {
     <div class="hlx-popup-header">
       <div>
         <h4>${config.label}</h4>
-        <div class="hlx-details">${config.status}${config.audience ? ', ' : ''}${config.audience}${config.variants[config.variantNames[0]].code.length ? ', Blocks: ' : ''}${config.variants[config.variantNames[0]].code.join(',')}</div>
+        <div class="hlx-details">${config.status}${config.audience ? ', ' : ''}${config.audience}${config.variants[config.variantNames[0]].blocks.length ? ', Blocks: ' : ''}${config.variants[config.variantNames[0]].blocks.join(',')}</div>
         <div class="hlx-info">How is it going?</div>
       </div>
       <div>
